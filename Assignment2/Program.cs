986434a2fc8a98199429f6d08b7bd19711c97c7f
matthiashulme <matthiashulme@MATTHIASHUL56ED>
2023-01-18 19:27:05 -0700

    Add project files.

---
+++ Assignment2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int[] rolls = new int[6];
+
+            // Simulate the roll of 1 die for 100 times
+            for (int i = 0; i < 100; i++)
+            {
+                rolls[r.Next(6)]++;
+            }
+
+            Console.WriteLine(rolls[4]);
+        }
+    }
+}
